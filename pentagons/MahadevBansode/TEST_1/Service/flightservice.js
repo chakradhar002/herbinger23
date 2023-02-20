402868112a2b097d1dee205b48906eaebdeb0b60
Mahadev Bansode <Mahadev.Bansode@harbingergroup.com>
2023-02-20 21:36:39 +0530

    test

--- pentagons/MahadevBansode/TEST_1/Service/flightservice.js
+++ pentagons/MahadevBansode/TEST_1/Service/flightservice.js
@@ -20,22 +20,28 @@ let createCustomer = (req, res) => {
   /**
    * @description used to check weather customer is already exists into the database system.
    */
-  let sql = `select * from customer where email='${email}'`;
-  connect.query(sql, function (err, result) {
-    if (err) throw err;
-    if (result.length > 0) {
-      res.send(JSON.stringify("You are already an existing Customer..!"));
-    }
-  });
+  try {
 
-  /**
-   * @description insert customer record into the customer table if it not present into the database.
-   */
-  sql = `insert into customer(email,firstname,lastname,gender,password) values('${email}','${fname}','${lname}','${gender}','${password}')`;
-  connect.query(sql, function (err, result) {
-    if (err) throw err;
-    res.send(JSON.stringify("Successfully Regestered..!"));
-  });
+    let sql = `select * from customer where email='${email}'`;
+    connect.query(sql, function (err, result) {
+      if (err) throw err;
+      if (result.length > 0) {
+        res.send(JSON.stringify("You are already an existing Customer..!"));
+      }
+    });
+
+    /**
+     * @description insert customer record into the customer table if it not present into the database.
+     */
+    sql = `insert into customer(email,firstname,lastname,gender,password) values('${email}','${fname}','${lname}','${gender}','${password}')`;
+    connect.query(sql, function (err, result) {
+      if (err) throw err;
+      res.send(JSON.stringify("Successfully Regestered..!"));
+    });
+
+  } catch (error) {
+    res.send(error);
+  }
 }
 /**
  * @description display all the availble flights for given source - destination on given date.
@@ -50,14 +56,20 @@ const getFlightsList = (req, res) => {
   //date given by user/customer.
   let date = req.body.date;
 
-  let sql = `select * from flight_details where sourceairport = '${source}' and destinationairport = '${destination}' and Date(departuredate) = '${date}'`;
+  try {
 
-  connect.query(sql, function (err, result) {
-    if (err) throw err;
+    let sql = `select * from flight_details where sourceairport = '${source}' and destinationairport = '${destination}' and Date(departuredate) = '${date}'`;
 
-    result.length ? res.status(200).json(result) : res.status(200).json(`No Flights Availble for '${source} to '${destination}' on '${date}`);
-    //console.log(result);
-  });
+    connect.query(sql, function (err, result) {
+      if (err) throw err;
+
+      result.length ? res.status(200).json(result) : res.status(200).json(`No Flights Availble for '${source} to '${destination}' on '${date}`);
+      //console.log(result);
+    });
+
+  } catch (error) {
+    res.send(error);
+  }
 };
 
 /**
@@ -77,20 +89,26 @@ const createReservation = (req, res) => {
   let ticket = req.body.tickettype;
   let meal = req.body.meal;
 
-  sql = `insert into passenger(email,visatype,passportno,tickettype) values('${email}','${visa}','${passport}','${ticket}')`;
+  try {
+    sql = `insert into passenger(email,visatype,passportno,tickettype) values('${email}','${visa}','${passport}','${ticket}')`;
 
-  connect.query(sql, function (err, result) {
-    if (err) throw err;
-    console.log("passenger = " + result);
-  });
+    connect.query(sql, function (err, result) {
+      if (err) throw err;
+      console.log("passenger = " + result);
+    });
 
-  sql = `insert into reservation(fno,email,seat_no,tickettype,meal) values('${fno}','${email}','${seat}','${ticket}','${meal}')`;
+    sql = `insert into reservation(fno,email,seat_no,tickettype,meal) values('${fno}','${email}','${seat}','${ticket}','${meal}')`;
 
-  connect.query(sql, function (err, result) {
-    if (err) throw err;
-    console.log("Ticket Booked successfully !", result.pid);
-    searchForResult(req, res);
-  });
+    connect.query(sql, function (err, result) {
+      if (err) throw err;
+      console.log("Ticket Booked successfully !", result.pid);
+      searchForResult(req, res);
+    });
+
+  } catch (error) {
+    res.send(error);
+  }
+
 };
 /**
  * @description change the reservation made by passenger with the new reservation Details.
@@ -107,14 +125,17 @@ const modifyReservation = (req, res) => {
   //ticket type which is passed through json body to change the reservation.
   let tickettype = req.body.tickettype;
   let meal = req.body.meal;
-
-  connect.query(
-    `update reservation set fno='${fno}',seat_no='${seat_no}',tickettype='${tickettype}',meal='${meal}' where rid='${rid}'`,
-    (err, result) => {
-      if (err) throw err;
-      getReservationDetails(req, res);
-    }
-  );
+  try {
+    connect.query(
+      `update reservation set fno='${fno}',seat_no='${seat_no}',tickettype='${tickettype}',meal='${meal}' where rid='${rid}'`,
+      (err, result) => {
+        if (err) throw err;
+        getReservationDetails(req, res);
+      }
+    );
+  } catch (error) {
+    res.send(error);
+  }
 };
 
 /**
@@ -129,12 +150,17 @@ const cancelReservation = (req, res) => {
 
   let sql = `update reservation set status='C' where rid='${rid}' and status='B'`;
 
-  connect.query(sql, (err, result) => {
-    if (err) throw err;
-    if (result.affectedRows > 0)
-      res.send(JSON.stringify("Your Flight ticket is Cancelled : id = " + rid));
-    else res.send(JSON.stringify("Recored not Found Having id = " + rid));
-  });
+  try {
+    connect.query(sql, (err, result) => {
+      if (err) throw err;
+      if (result.affectedRows > 0)
+        res.send(JSON.stringify("Your Flight ticket is Cancelled : id = " + rid));
+      else res.send(JSON.stringify("Recored not Found Having id = " + rid));
+    });
+  } catch (error) {
+    res.send(error);
+  }
+
 };
 
 /**
@@ -146,10 +172,15 @@ const getReservationDetails = (req, res) => {
   //passenger id which is passed through end point to show reservation details.
   let rid = req.params.rid;
 
-  connect.query(`select * from reservation where rid='${rid}'`, (err, result) => {
-    if (err) throw err;
-    res.status(200).json(result);
-  });
+  try {
+    connect.query(`select * from reservation where rid='${rid}'`, (err, result) => {
+      if (err) throw err;
+      res.status(200).json(result);
+    });
+  } catch (error) {
+    res.send(error);
+  }
+
 };
 /**
  * @description display all the lists of availble food options in flight.
@@ -158,10 +189,15 @@ const getReservationDetails = (req, res) => {
  */
 const getMealMenu = (req, res) => {
 
-  connect.query(`select * from meal`, (err, result) => {
-    if (err) throw err;
-    res.status(200).json(result);
-  });
+  try {
+    connect.query(`select * from meal`, (err, result) => {
+      if (err) throw err;
+      res.status(200).json(result);
+    });
+  } catch (error) {
+    res.send(error);
+  }
+
 };
 /**
  * @description used to return inserted data into the reservation table for Acknowledgement
@@ -172,14 +208,19 @@ const getMealMenu = (req, res) => {
 let searchForResult = (req, res) => {
   let email = req.body.email;
   let fno = req.body.fno;
-  connect.query(
-    `select * from reservation where email='${email}' and fno='${fno}'`,
+  try {
+    connect.query(
+      `select * from reservation where email='${email}' and fno='${fno}'`,
 
-    (err, result) => {
-      if (err) throw err;
-      res.status(200).json(result);
-    }
-  );
+      (err, result) => {
+        if (err) throw err;
+        res.status(200).json(result);
+      }
+    );
+  } catch (error) {
+    res.send(error);
+  }
+
 };
 
 
@@ -192,3 +233,5 @@ module.exports = {
   getMealMenu,
   createCustomer
 };
+
+

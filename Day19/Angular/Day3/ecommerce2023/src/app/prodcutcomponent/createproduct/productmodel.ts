32fb825783c66d4c5fdb446a709e0fe4a872598b
unknown <mantha747@gmail.com>
2023-02-22 15:54:05 +0530

    ecommerce

---
+++ Day19/Angular/Day3/ecommerce2023/src/app/prodcutcomponent/createproduct/productmodel.ts
@@ -0,0 +1,7 @@
+export interface Product {
+    productID: string;
+    productname: string;
+    productdescritpion: string;
+    qunatity: string;
+    unitprice: string;
+ }
---
+++ Day19/Angular/Day3/ecommerce2023/src/app/productcategory/productcategory.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-productcategory',
+  templateUrl: './productcategory.component.html',
+  styleUrls: ['./productcategory.component.css']
+})
+export class ProductcategoryComponent {
+
+}

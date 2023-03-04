cbc1a148c681beaf9010fe2c592f6f586df5895d
unknown <mantha747@gmail.com>
2023-03-04 15:29:21 +0530

    for reference

--- Day26/crud- insertion/react-frontend/src/App.js
+++ Day26/crud- insertion/react-frontend/src/App.js
@@ -1,6 +1,4 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 import {BrowserRouter as Router, Route, Switch} from 'react-router-dom'
 import CreateEmployeeComponent from './components/CreateEmployeeComponent';
 
@@ -8,17 +6,17 @@ function App() {
   return (
     <div>
         <Router>
-              <HeaderComponent />
+             
                 <div className="container">
                     <Switch> 
                           
                        
                           <Route path = "/add-employee/:id" component = {CreateEmployeeComponent}></Route>
                          
-                          {/* <Route path = "/update-employee/:id" component = {UpdateEmployeeComponent}></Route> */}
+                        
                     </Switch>
                 </div>
-              <FooterComponent />
+            
         </Router>
     </div>
     
